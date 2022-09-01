a968a397350f9f85f60d6168e90ce31bf9bdcbc0
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-09-01 21:08:29 +0300

    setting up fetch request in getMealList callback

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,14 @@ searchBtn.addEventListener("click", getMealList);
 // get meal list for provided ingredient(s)
 function getMealList() {
     let searchInput = document.getElementById("search-input").value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.foreach(meal => {
+                ;
+            });
+        }
+    })
 }
