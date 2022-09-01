e5f9c0d79791a24804f0f7c877533a73437088b2
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-09-01 20:56:55 +0300

    adding callback to searchBtn

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,8 @@ const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
 // searchBtn event listener
 searchBtn.addEventListener("click", getMealList);
+
+// get meal list for provided ingredient(s)
+function getMealList() {
+    let searchInput = document.getElementById("search-input").value.trim();
+}
