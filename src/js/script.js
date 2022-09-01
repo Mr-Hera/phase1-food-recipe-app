828b7b2b241e9a855a59b6ecda3e4530364055ab
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-09-01 20:42:10 +0300

    added searchBtn event listener

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,5 @@ const mealList = document.getElementById("meal");
 const mealDetailsContent = document.querySelector(".meal-details-content");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
+// searchBtn event listener
+searchBtn.addEventListener("click", getMealList);
